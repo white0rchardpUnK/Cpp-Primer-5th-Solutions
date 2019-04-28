c8ccac85e93384c19d110a798bb851724352659f
raymain <raymain1944@gmail.com>
2019-04-28 20:48:57 +0800

    devotion, keep going~

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <vector>
+using std::cin; using std::cout;
+using std::endl;
 
-using std::cin; using std::cout; using std::endl;
-using std::vector;
-
-int main()
-{
-    vector<int> a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (auto i : a){
-        cout << (i % 2 != 0 ? i * 2 : i) << " ";
-    }
+int main(){
+    unsigned char a = 'q';
+    a << 6;
+    cout << a << endl;
 }
