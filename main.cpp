441ff919d0cdf3727b2ac94c241d0d0f688efc04
raymain <raymain1944@gmail.com>
2019-05-12 19:14:21 +0800

    Revert "Revert "8th chapter finished, keep going~"" fix the wrong chapter 9th to 8th~ This reverts commit 9446b34e

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <list>
+#include <deque>
+#include <string>
+#include <sstream>
 
 using std::cin; using std::cout; using std::endl;
-using std::string;
+using std::string; using std::list; using std::deque;
 
-int main(){
-    
+int main() {
+    cout << "Please type some number for list<int>: ";
+    list<int> l;
+    string s;
+    getline(cin, s);
+    std::istringstream is(s);
+    int i;
+    while (is >> i)
+        l.push_front(i);
+
+    deque<int> odd;
+    deque<int> even;
+    for (auto lb = l.cbegin(), le = l.cend(); lb != le; ++lb){
+        if (*lb % 2 != 0){
+            odd.push_front(*lb);
+        } else {
+            even.push_back(*lb);
+        }
+    }
+
+    for (const auto &o : odd)
+        cout << o << " ";
+    cout << endl;
+    for (const auto &e : even)
+        cout << e << " ";
 }
