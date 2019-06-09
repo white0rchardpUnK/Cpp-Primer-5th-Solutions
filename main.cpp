20553c0db4b791ea63ef879df9e395d118da8f4b
raymain <raymain1944@gmail.com>
2019-06-09 22:33:21 +0800

    6.9

--- main.cpp
+++ main.cpp
@@ -3,23 +3,12 @@ using std::cin; using std::cout; using std::endl; using std::cerr;
 #include <string>
 using std::string;
 #include <memory>
-using std::shared_ptr; using std::make_shared;
-
-struct destination;
-struct connection;
-
-connection connect(destination *);
-
-void disconnect(connection);
-
-void end_connection(connection *p){ disconnect(*p);}
-
-void f(destination &d /*其它参数*/){
-    connection c = connect(&d);
-    shared_ptr<connection>p(&c, [](connection *p){disconnect(*p);});
-    //使用连接
-    //当f退出时（即使是由于异常），connection会被正确关闭
-}
+using std::unique_ptr;
 
 int main() {
+    int ix = 1024;
+    int *pi = &ix;
+    int *pi2 = new int(2048);
+
+    typedef unique_ptr<int> IntP;
 }
